7c25798be07bd81a67a269377799d4b256907554
parteek jain <102279288+parteekjain99@users.noreply.github.com>
2022-05-24 14:18:43 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,7 +1,9 @@
 const express=require('express') 
 const router= express.Router()
 const userController=require('../Controller/userController')
+const valid = require('../middleware/valid');
 
 router.post('/User',userController.createUser)
+router.post('/login', valid.validLogin , userController.userLogin)
 
 module.exports=router;
