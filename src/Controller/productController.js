4307e81e9b3ec12d2528b1e42e4c5c7c71f0eb80
Harsh Shrivastav <101973697+harshshriva@users.noreply.github.com>
2022-05-26 11:25:32 +0530

    Update productController.js

--- src/Controller/productController.js
+++ src/Controller/productController.js
@@ -34,5 +34,64 @@ const createproducts = async function(req, res) {
 
 
 }
+const getProductBYQuery = async function(req, res) {
 
-module.exports = {createproducts}
+    try {
+        if (req.query.size || req.query.name || req.query.priceGreaterThan || req.query.priceLessThan) {
+            let availableSizes = req.query.size
+            let title = req.query.name
+            let priceGreaterThan = req.query.priceGreaterThan
+            let priceLessThan = req.query.priceLessThan
+            obj = {}
+            if (availableSizes) {
+                obj.availableSizes = availableSizes
+            }
+            if (title) {
+                obj.title = { $regex: title, $options: " " }
+            }
+            if (priceGreaterThan) {
+                obj.price = { $gt: priceGreaterThan }
+            }
+            if (priceLessThan) {
+                obj.price = { $lt: priceLessThan }
+            }
+            obj.isDeleted = false
+            obj.deletedAt = null
+
+            if (req.query.sort === -1) {
+                const getProductsList = await productModel.find(obj).sort({ price: -1 })
+
+                if (!getProductsList || getProductsList.length == 0) {
+                    res.status(400).send({ status: false, message: `product is not available right now.` })
+                } else {
+                    res.status(200).send({ status: true, message: 'Success', data: getProductsList })
+                }
+
+            } else {
+                const getProductsList = await productModel.find(obj).sort({ price: 1 })
+
+                if (!getProductsList || getProductsList.length == 0) {
+                    res.status(400).send({ status: false, message: `product is not available right now.` })
+                } else {
+                    res.status(200).send({ status: true, message: 'Success', data: getProductsList })
+                }
+            }
+
+
+        } else {
+            if (req.query.sort === -1) {
+                const getListOfProducts = await productModel.find({ isDeleted: false, deletedAt: null }).sort({ price: -1 })
+                res.status(200).send({ status: true, message: 'Success', data: getListOfProducts })
+            } else {
+                const getListOfProducts = await productModel.find({ isDeleted: false, deletedAt: null }).sort({ price: 1 })
+                res.status(200).send({ status: true, message: 'Success', data: getListOfProducts })
+            }
+        }
+    } catch (err) {
+        res.status(500).send({ status: false, msg: err.message })
+    }
+
+}
+
+
+module.exports = {createproducts, getProductBYQuery}
